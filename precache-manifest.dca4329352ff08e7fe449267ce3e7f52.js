24751ce774e1221ccd5eb7b45c768db9e4416c6f
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2022-10-20 14:44:48 -0700

    Updates

---
+++ precache-manifest.dca4329352ff08e7fe449267ce3e7f52.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b9ad0c3b58202509d57",
+    "url": "/helping-hands/static/css/main.99272935.chunk.css"
+  },
+  {
+    "revision": "0b9ad0c3b58202509d57",
+    "url": "/helping-hands/static/js/main.0b9ad0c3.chunk.js"
+  },
+  {
+    "revision": "7fe48ce2d691ceafc699",
+    "url": "/helping-hands/static/js/1.7fe48ce2.chunk.js"
+  },
+  {
+    "revision": "f537a41a46685d35557c",
+    "url": "/helping-hands/static/js/runtime~main.f537a41a.js"
+  },
+  {
+    "revision": "a81ae9d1e7cd30ca041224e01eb17a49",
+    "url": "/helping-hands/static/media/treatment-image-1.a81ae9d1.jpg"
+  },
+  {
+    "revision": "6d425e638294d27db7bacfbc38a5426b",
+    "url": "/helping-hands/static/media/aetna.6d425e63.png"
+  },
+  {
+    "revision": "b1a115cb567510e8ec14e6b05ef8bdde",
+    "url": "/helping-hands/static/media/beacon.b1a115cb.png"
+  },
+  {
+    "revision": "a685f0cda3e3d8e2f530c9ce30865aa5",
+    "url": "/helping-hands/static/media/bluecross.a685f0cd.png"
+  },
+  {
+    "revision": "6defc1965a08a859708e60937009ca63",
+    "url": "/helping-hands/static/media/cigna.6defc196.png"
+  },
+  {
+    "revision": "0f02133cd75513a05f985c5bbe97271a",
+    "url": "/helping-hands/static/media/hero-vbg2.0f02133c.mp4"
+  },
+  {
+    "revision": "986bc209348281e47f9a175b616f9447",
+    "url": "/helping-hands/static/media/logo-wh.986bc209.png"
+  },
+  {
+    "revision": "8718ece957f1a19ac67886f6c2a297d0",
+    "url": "/helping-hands/static/media/logo.8718ece9.png"
+  },
+  {
+    "revision": "3440ad860a5eb96efa01d65c136aecc3",
+    "url": "/helping-hands/static/media/mental-illness.3440ad86.png"
+  },
+  {
+    "revision": "fbeb56fb1286ccea01afc386877ac4fa",
+    "url": "/helping-hands/static/media/substance-use.fbeb56fb.png"
+  },
+  {
+    "revision": "e43d2b5528c1d0503918199e532fceaa",
+    "url": "/helping-hands/static/media/addiction-treatment.e43d2b55.png"
+  },
+  {
+    "revision": "2cf97747278a8f166be1d8a3c958eb0c",
+    "url": "/helping-hands/static/media/employment-assistance.2cf97747.png"
+  },
+  {
+    "revision": "85be90302f274aafe7c9bee104ce612a",
+    "url": "/helping-hands/static/media/therapy2.85be9030.jpg"
+  },
+  {
+    "revision": "38b750562d3162d593836104fe332944",
+    "url": "/helping-hands/static/media/staff.38b75056.jpg"
+  },
+  {
+    "revision": "da98d45a05a0da0f0e4d27a2b66ea76f",
+    "url": "/helping-hands/static/media/clinic.da98d45a.jpg"
+  },
+  {
+    "revision": "3dc594eb44086fb3004af0b556a15818",
+    "url": "/helping-hands/static/media/employment-assistance2.3dc594eb.png"
+  },
+  {
+    "revision": "3b35ae449e240b2ccc55c2a5c8d35617",
+    "url": "/helping-hands/static/media/team-image-1.3b35ae44.jpg"
+  },
+  {
+    "revision": "e3036db9350d49f97d7bc61110def10b",
+    "url": "/helping-hands/static/media/team-image-2.e3036db9.jpg"
+  },
+  {
+    "revision": "113683f2ab207479764bdd1181154768",
+    "url": "/helping-hands/static/media/team-image-3.113683f2.jpg"
+  },
+  {
+    "revision": "6cec3afc7fb7fc7e27492eeada612118",
+    "url": "/helping-hands/static/media/team-image-4.6cec3afc.jpg"
+  },
+  {
+    "revision": "30ffc312a1529d5117e94163c4ba03ac",
+    "url": "/helping-hands/static/media/team-image-5.30ffc312.jpg"
+  },
+  {
+    "revision": "69b4dbd7ed1033288e306d109f39cee1",
+    "url": "/helping-hands/static/media/team-image-6.69b4dbd7.jpg"
+  },
+  {
+    "revision": "726f644e55b1610ea4792c5d89b57bff",
+    "url": "/helping-hands/static/media/molina-healthcare.726f644e.png"
+  },
+  {
+    "revision": "f64380adb288f823da65111d09c715c9",
+    "url": "/helping-hands/static/media/mhn.f64380ad.png"
+  },
+  {
+    "revision": "caa531b2f260498491cb708dfef0f67f",
+    "url": "/helping-hands/static/media/united-healthcare.caa531b2.png"
+  },
+  {
+    "revision": "824ca03ed1c8be62014f43ca59cc2f2d",
+    "url": "/helping-hands/static/media/footer-bg.824ca03e.jpg"
+  },
+  {
+    "revision": "36734c69ee39d7ba89e6a1e0c3da6f06",
+    "url": "/helping-hands/static/media/testimonials-bg.36734c69.jpg"
+  },
+  {
+    "revision": "3d0c37a61f22c16514426d04b5661b11",
+    "url": "/helping-hands/static/media/treatment-bg2.3d0c37a6.jpg"
+  },
+  {
+    "revision": "11b5b8caff037c94520bf9d0abed1776",
+    "url": "/helping-hands/static/media/mission-bg.11b5b8ca.jpg"
+  },
+  {
+    "revision": "6c5014073901bd0cc801e6138837f4a6",
+    "url": "/helping-hands/static/media/about-us-bg.6c501407.jpg"
+  },
+  {
+    "revision": "594d7b80ae2c4f0f987fe866b71e3061",
+    "url": "/helping-hands/static/media/insurance-bg.594d7b80.jpg"
+  },
+  {
+    "revision": "a271a9991fdec461aa7ac8fb829e0300",
+    "url": "/helping-hands/static/media/contact-bg.a271a999.jpg"
+  },
+  {
+    "revision": "e8e093d420abd8d09981c05cabdbd6d8",
+    "url": "/helping-hands/index.html"
+  }
+];
