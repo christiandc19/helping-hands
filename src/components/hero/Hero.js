dfc96c8a687de8940091021776b146997cd520ae
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-02-13 16:56:55 -0800

    replaced files

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,38 +3,49 @@ import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 import './Hero.css'
 
-import HeroLogo from '../../assets/heroLogo.png'
+// import BgVideo from '../../assets/hero-vbg.mp4'
+
+import waves from '../../assets/waves.svg'
 
 
 
 const Hero = () => {
     return (
-        <>
+<>        
+
+
+
         <div className='hero'>
+
+        <div className='hero-divider'>
+                <img src={waves} loading="lazy" repeat-x alt='wave divider'/>
+            </div>
+
+
             <div className="hero-container">
                 <div className="content">
-
-                <div className='hero-logo'>
+                        <h1 className="animate-charcter">HELPING HANDS REHAB</h1>
                     <Fade left>
-                    <img src={HeroLogo} width="280px" height="220px" alt="Buena Park Logo" loading="lazy"/>
+                        <h2 className='subtitle'>Substance Abuse Addiction and <br/>Mental Health Treatment Facility</h2>
                     </Fade>
+                        <br />
+                        <div className='hero-btn'>
+                        <Link to="/contact">
+                        <button>Get an Appointment</button>
+                    </Link>
                     </div>
-                    
-                <Fade top><h1>HELPING HANDS</h1></Fade> 
-                <Fade bottom>
-                    <h2 className='subtitle'>SUBSTANCE ABUSE AND MENTAL HEALTH TREATMENT FACILITY</h2>
-                    <p>Find your happiness and learn to put it first.</p>
-                </Fade>
-
-                    <Link to="/treatment"><button>Learn More</button></Link>
+                    <br />
+                    <Fade bottom>
+                        <p>Find your happiness and learn to put it first.</p>
+                    </Fade>
                 </div>
             </div>
         </div>
 
 
-        </>
+</>
+
     )
 }
 
 export default Hero
-
